50a330a6a1b1ed81d7282bc1e81a9dc3a84eaae7
Pujitha Paladugu <pujitha.rathna@gmail.com>
2026-09-17 13:48:49 +0000

    fix(binder): fail closed when hamicore cannot calculate GPU memory limit (#2157)
    Signed-off-by: Pujitha Paladugu <10557236+pujitha24@users.noreply.github.com>
    Co-authored-by: Pujitha Paladugu <10557236+pujitha24@users.noreply.github.com>

--- pkg/binder/plugins/hamicore/hami_core.go
+++ pkg/binder/plugins/hamicore/hami_core.go
@@ -39,7 +39,8 @@ func (p *Plugin) PreBind(
 
 	cudaDeviceMemoryLimit, err := calculateCudaDeviceMemoryLimit(node, bindRequest)
 	if err != nil {
-		return nil
+		return fmt.Errorf("failed to calculate CUDA_DEVICE_MEMORY_LIMIT for pod <%s/%s>: %w",
+			pod.Namespace, pod.Name, err)
 	}
 
 	containerRef, err := resources.GetFractionContainerRef(pod)
